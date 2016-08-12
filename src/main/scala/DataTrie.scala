18a8fa1bb5d285f6740f8b41c388993c0513aec5
himanshuarora <himanshu.arora.alw@gmail.com>
2016-08-13 01:40:20 +0200

    First version of autocomplete

---
+++ src/main/scala/DataTrie.scala
@@ -0,0 +1,69 @@
+import scala.collection.immutable.TreeMap
+
+/**
+ * A prefix tree (trie) data structure used to store the string data to perform efficient prefix search.
+ * It can be used to provide auto-complete solutions backed by a fast data search.
+ * For more details refer https://en.wikipedia.org/wiki/Trie
+ * @param character key of the nodes, None for root node
+ * Created by arorah on 12/08/2016.
+ */
+class DataTrie(character: Option[Char]) {
+
+  /**
+   * To store the children of a node using TreeMap data structure.
+   * TreeMap stores data in lexical/alphabetical order.
+   */
+  var children = new TreeMap[Char, DataTrie]
+  /**
+   * While adding a word to this trie, the last character node will not have any children. So storing the whole word with this node.
+   * Needed while returning the matched string as a result of prefix search.
+   * Only leaf node will contain the value in `mayBeWord`.
+   */
+  var mayBeWord: Option[String] = None
+
+  //Function to insert new word in trie
+  def addData(data: String): DataTrie = add(data, data.toLowerCase.replaceAll(" ", ""))  //Lowercase called to make search case-insensitive and there is no need to store spaces as nodes
+
+  /**
+   * This function inserts a word character by character.
+   * For each char of the data, it checks if the char has already been inserted.
+   * If so then there is no need to insert that char but rather call recursive this same function to insert the remain chars as the children of the node of current char.
+   * But if the current char has not already been inserted then this function creates a new node for that char and inserts it in the TreeMap and then calls recursively to
+   * itself to insert the remain chars as children of this last inserted node.
+   * @param data a copy of original data, used to set `mayBeWord` for leaf nodes
+   * @param suffix suffix of the word to be inserted
+   */
+  private def add(data: String, suffix: String): DataTrie = {
+    suffix.toList match {
+      case Nil => {
+        this.mayBeWord = Some(data)
+        this
+      }
+      case head::tail => {
+        this.children.get(head).fold{
+          val newNode = new DataTrie(Some(head))
+          this.children = this.children.insert(head, newNode)
+          newNode.add(data, tail.mkString)
+        }(_.add(data, tail.mkString))
+      }
+    }
+  }
+
+  //Find all the node who has the input data as prefix
+  private def findNode(data: String): Option[DataTrie] = data.toLowerCase.replaceAll(" ", "").toList match {
+    case Nil => Some(this)
+    case head::tail => this.children.get(head).flatMap(_.findNode(tail.mkString))
+  }
+
+  //To find all the leaf nodes containing complete words
+  private def findAllWords: List[String] = {
+    this.mayBeWord.toList ++ this.children.values.flatMap(_.findAllWords)
+  }
+
+  //Prefix search function, returns list of matched words
+  def search(text: String): List[String] = {
+    findNode(text).toList.flatMap(_.findAllWords)
+  }
+
+}
+
---
+++ src/main/scala/Runner.scala
@@ -0,0 +1,33 @@
+import scala.collection.immutable.{TreeSet, WrappedString}
+
+/**
+ * Program runner object.
+ * Created by arorah on 12/08/2016.
+ */
+
+
+object Runner extends App {
+  //Instantiating and inserting data into the trie data structure
+  val dateTrie = new DataTrie(None)
+  val inputDataList = List("Pandora", "Pinterest", "Paypal", "Pg&e", "Project free tv Priceline", "Press democrat",
+    "Progressive", "Project runway", "Proactive", "Programming", "Progeria", "Progesterone", "Progenex", "Procurable",
+    "Processor", "Proud", "Print", "Prank", "Bowl", "Owl", "River", "Phone", "Kayak", "Stamps", "Reprobe"
+  )
+  inputDataList.foreach(dateTrie.addData(_))
+
+  //Infinite loop to let user search as many time as he/she wants
+  while(true) {
+    println("Either enter the text to be searched to just type Ctrl+C to stop")
+    val input = scala.io.StdIn.readLine()
+    if(input != null) {
+      val t0 = System.nanoTime()
+      //Firing the search algo
+      val res = dateTrie.search(input)
+      val t1 = System.nanoTime()
+      println("Found matches are as following : ")
+      res.take(Math.min(4, res.length)).foreach(println)
+      println("Total execution time = " + (t1 - t0) / 1000000000.0 + " Sec")
+    }
+  }
+
+}
